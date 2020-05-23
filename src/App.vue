f924244a61cbdbfb9167a6b1b254bb81834001b0
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-05-23 23:14:09 +0900

    create markdown-tool minimum necessary

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <markDownList msg="Markdown List"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import markDownList from './components/markDownList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    markDownList
   }
 }
 </script>
---
+++ src/components/markDownList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Markdown 早見表</h3>
+    <table border= "1" class= "markdown_table">
+      <tr>
+        <th>希望の表示</th>
+        <th>マークダウン</th>
+      </tr>
+      <tr>
+        <th>コード</th>
+        <th>```</th>
+      </tr>
+      <tr>
+        <th>インライン表示</th>
+        <th>`(ここに文字を記載)`</th>
+      </tr>
+      <tr>
+        <th>強調(太字)</th>
+        <th>**(ここに文字を記載)**</th>
+      </tr>
+      <tr>
+        <th>強調(斜体)</th>
+        <th>*(ここに文字を記載)*</th>
+      </tr>
+      <tr>
+        <th>チェックボックス(チェック済)</th>
+        <th>- [x] チェックボックス</th>
+      </tr>
+      <tr>
+        <th>引用</th>
+        <th>> text</th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'markDownList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  color: red;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.markdown_table {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
